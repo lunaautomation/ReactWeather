38df997532b195c212e9aa76935f9872f10aef5f
Dan Hitchcock <33898935+lunaautomation@users.noreply.github.com>
2020-01-14 15:11:27 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import WeatherCard from './WeatherCard';
+import APIresult from './api.js';
 const items = [{day:"Mon", image:"sun"},{day:"Tue", image:"rain"}]
 
-
 function App() {
+   
   return (
     <div className="App">
+      <APIresult/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>some code</code> and save to reload. yo ho ho.
-        </p>        
         {items.map(thing =>(<WeatherCard className="App-logo" day={thing.day}image={thing.image}></WeatherCard>))}
       </header>
     </div>
