78a5faf18f1ffce62514744188ba8906d61a0930
Dan Hitchcock <33898935+lunaautomation@users.noreply.github.com>
2020-01-21 15:39:30 +0000

    Add files via upload

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import APIresult from './api.js';
 
+
 class App extends React.Component {
   constructor(props) {
     super(props);
--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import WeatherCard from './WeatherCard';
 
+
 class APIresult extends React.Component {
   constructor(props) {
     super(props);
@@ -64,7 +65,7 @@ class APIresult extends React.Component {
       return (
         <span className="Block" >          
           {items ? items.map(item => (
-    <WeatherCard day={item.dt_txt}image={item.weather[0].description}description={item.weather[0].description}temp={item.main.temp}></WeatherCard>           
+    <WeatherCard day={item.dt_txt}image={item.weather[0].description}description={item.weather[0].description}temp={item.main.temp}windDir={item.wind.deg}windSpd={item.wind.speed}></WeatherCard>           
           )):<p fontSize="30px">OH NO ITS EMPTY!</p>}
         </span>
       );
