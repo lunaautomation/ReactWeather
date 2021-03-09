bc98358083bb3e2d30ecc50c6793e623d484baf7
Dan Hitchcock <33898935+lunaautomation@users.noreply.github.com>
2021-03-09 21:34:06 +0000

    update the naff test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders a beg to enter a location', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/enter a location/i);
   expect(linkElement).toBeInTheDocument();
 });
